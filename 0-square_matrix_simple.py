403e49c760ab807f19bda8d7be7ca0b123a3dcef
Thecodingyogi <cynthiatonui@gmail.com>
2023-03-15 12:53:04 -0700

    corrected

--- 0-square_matrix_simple.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    new_matrix = [[y**2 for y in x] for x in matrix]
-    return new_matrix
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,6 +4,7 @@ def uniq_add(my_list=[]):
     uniq_list = set(my_list)
 
     for x in uniq_list:
+        sum = 0
         sum += x
 
     return sum
