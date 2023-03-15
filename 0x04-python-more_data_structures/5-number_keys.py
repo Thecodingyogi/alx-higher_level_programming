e8ffe2a80e7d4df968c158618ea73e5f2ff2945b
Thecodingyogi <cynthiatonui@gmail.com>
2023-03-15 13:40:45 -0700

    Fixed

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    sum = 0
-
-    key_list = list(a_dictionary.keys())
-
-    for i in key_list:
-        sum += i
-    return sum
+    return len(a_dictionary)
