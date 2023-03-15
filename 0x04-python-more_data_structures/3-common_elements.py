90ef3acd315f8d1d1504c5e88355ff780c3bf356
Thecodingyogi <cynthiatonui@gmail.com>
2023-03-15 13:25:47 -0700

    correction

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    list_1 = set(set_1)
-    list_2 = set(set_2)
-
-    if list_1 and list_2:
-        return list_1 and list_2
+    return set_1 and set_2
