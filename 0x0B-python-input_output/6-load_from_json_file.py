9b6f3b702891f0cf394711331865dc1695f9552a
Thecodingyogi <cynthiatonui@gmail.com>
2023-04-12 07:48:05 -0700

    Create object from JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/binpython3
+
+"""Defines a function that creates an object"""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an object from a JSON file"""
+    with open(filename, encoding='utf-8') as file:
+        return json.load(file)
