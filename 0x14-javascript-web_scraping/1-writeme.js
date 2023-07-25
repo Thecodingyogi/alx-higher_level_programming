2bdeceb35f5d6ed44a61dc4fd85fcd1ede01d303
Thecodingyogi <cynthiatonui@gmail.com>
2023-07-25 04:28:03 -0700

    Write a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (error) => {
-	console.log(error || process.argv[3])}
-);
+	if (error) console.log(error);
+});
