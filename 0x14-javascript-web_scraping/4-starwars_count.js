38d9dbebb981cfcd0d2d507869c24473328b0d20
Thecodingyogi <cynthiatonui@gmail.com>
2023-07-25 07:31:22 -0700

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,13 @@
 const request = require('request');
 const characterId = 18;
 const API_URL = `https://swapi-api.alx-tools.com/api/people/${characterId}/`;
-request(process.argv[2], (error, response, body) => {
+request.get(process.argv[2], (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
     const results = JSON.parse(body).results;
     const moviesWithWedgeAntilles = results.filter((film) =>
       film.characters.includes(API_URL));
-    console.log(moviesWithWedgeAntilles.length);
+    console.log(`${moviesWithWedgeAntilles.length}`);
   }
 });
