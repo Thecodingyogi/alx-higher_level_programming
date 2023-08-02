9acb0ddcc5183985ba1fac4f9232c2250b4096d1
Thecodingyogi <cynthiatonui@gmail.com>
2023-08-02 09:08:17 -0700

    JQuery Tasks

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+const movies = data.results;
+$.each(movies function (index, movie) {
+$('UL#list_movies').append('<li>' + movie.title + '</li>')});
+});
